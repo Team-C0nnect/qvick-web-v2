23d5a4c524067c3c4aa7e69eb4a150b586d14802
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-01-05 20:32:14 +0900

    :sparkles: Feature :: 검색기능 추가
    빨리 서버 가지고 싶어요

--- Qvick/src/Components/Main/Main.jsx
+++ Qvick/src/Components/Main/Main.jsx
@@ -6,6 +6,12 @@ export default function Main() {
     return (
         <div className="Main">
             <Head />
+            <div className="Search">
+                <input className="Search_Box" 
+                placeholder="원하시는 학생의 정보를 입력해주세요."></input>
+                <input type="button" className="Search_icon"></input>
+                <div className="Searcg_icon_line"></div>
+            </div>
         </div>
     )
 }
