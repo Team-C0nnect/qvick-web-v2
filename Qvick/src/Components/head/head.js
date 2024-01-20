de88da5feedf376ea60d9459b9a9b994b3db1bb6
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-01-20 21:45:37 +0900

    :sparkles: Feature :: head바 적용

--- Qvick/src/Components/head/head.js
+++ Qvick/src/Components/head/head.js
@@ -1,11 +1,12 @@
 import "./head.css";
 import React, { useEffect, useState } from "react";
+import Logo from "../../Assets/img/logo.png";
 
 export default function Head() {
     return (
         <div className="Head">
             <div className="HeadTitleWrap">
-                <span className="HeadTitle">Qvick</span>
+                <img className="Logo" src={Logo} alt="이미지"></img>
             </div>
         </div>
     )
