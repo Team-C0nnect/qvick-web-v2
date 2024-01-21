37a02e63b47963d3521e4f0d4a502a993344494a
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-01-21 21:45:51 +0900

    :lipstick: Fix :: 디자인수정

--- Qvick/src/Components/SideBar/Sidebar.jsx
+++ Qvick/src/Components/SideBar/Sidebar.jsx
@@ -4,7 +4,11 @@ export default function SideBar() {
     return (
         <div className="SideWrap">
             <div className="SideBar">
-
+                <div className="SideM">
+                    <div className="MemberWrap">구성원</div>
+                    <div className="CheckWrap">출석</div>
+                    <div className="HomeWrap">외출/외박</div>
+                </div>
             </div>
         </div>
     )
