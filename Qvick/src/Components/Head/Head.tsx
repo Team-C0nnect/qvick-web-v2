37fc88553905f076bdd079393685ba46bf061c98
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-01-25 00:48:47 +0900

    :memo: Fix :: 코드컨벤션 적용

---
+++ Qvick/src/Components/Head/Head.tsx
@@ -0,0 +1,13 @@
+import "./head.css";
+import React, { useEffect, useState } from "react";
+import Logo from "src/Assets/img/logo.png";
+
+export default function Head() {
+    return (
+        <div className="Head">
+            <div className="HeadTitleWrap">
+                <img className="Logo" src={Logo} alt="이미지"></img>
+            </div>
+        </div>
+    )
+}
