ed7d5f7e5450daa848dc4f85f846dc0d5c8ab64d
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-01-25 01:06:35 +0900

    :zap: Feature :: Side추가 및 경로수정

--- Qvick/src/App.tsx
+++ Qvick/src/App.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './Components/Login/Login';
-import Main from './Components/Main/Main';
-import Head from './Components/Head/Head';
+import Login from "src/Components/Login/Login";
+import Main from "src/Components/Main/Main";
+import Head from "src/Components/Head/Head";
+import Side from "src/Components/SideBar/Sidebar";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Login />} />
           <Route path="/Main" element={<Main />} />
           <Route path="/Head" element={<Head />} />
+          <Route path="/Side" element={<Side />} />
         </Routes>
       </div>
     </BrowserRouter>
