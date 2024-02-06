d36ae1f92cf3788182a2181d2f4fb0f472c82708
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-02-07 01:43:39 +0900

    :zap: Fix : style-components 활성화

---
+++ Qvick/src/Components/Search/Search.Style.tsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const SearchBox = styled.div`
+    position: relative;
+    display: flex;
+    width: 400px;
+    height: 200px;
+`;
+export const Search = styled.input`
+    position: absolute;
+`;
+export const ResultWrap = styled.div`
+    position: absolute;
+`;
