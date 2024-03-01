d7309210a9a9d24977eb432c4b2a6d5544cae33d
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-03-01 23:13:54 +0900

    :zap: Feature :: 로그아웃

--- Qvick/src/Components/Head/Name/Name.tsx
+++ Qvick/src/Components/Head/Name/Name.tsx
@@ -1,13 +1,19 @@
 import { useNavigate } from "react-router-dom";
 import React from "react";
 import * as S from "src/Components/Head/Name/Name.Style"
+import Login from "src/Components/Login/Login";
 
 export default function Name() {
     const navigate = useNavigate();
     return(
         <S.NameWrap>
             <S.Name>관리자님</S.Name>
-            <S.logoOut>로그아웃</S.logoOut>
+            <S.logoOut
+                onClick={() => {
+                    navigate("/")
+                }}>
+                로그아웃
+            </S.logoOut>
         </S.NameWrap>
     )
 }
