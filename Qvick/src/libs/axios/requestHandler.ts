bca157ef911eb30e2f79018de27d1116bb035d38
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-05-07 22:59:07 +0900

    :sparkles: Feature :: libs axios

---
+++ Qvick/src/libs/axios/requestHandler.ts
@@ -0,0 +1,21 @@
+import { AxiosRequestConfig } from "axios";
+import {
+    ACCESS_TOKEN_KEY,
+    REFRESH_TOKEN_KEY,
+    REQUEST_TOKEN_KEY,
+} from "src/constants/tokens/token.constants"
+
+import Token from "../token/token";
+
+export const requestHandler = (config: AxiosRequestConfig) => {
+    const access_token = Token.getToken(ACCESS_TOKEN_KEY);
+    const refresh_token = Token.getToken(REFRESH_TOKEN_KEY);
+
+    if (access_token || refresh_token) {
+        config.headers = {
+            "Content-Type": "application/json",
+            [REFRESH_TOKEN_KEY]: `Bearer ${Token.getToken(ACCESS_TOKEN_KEY)}`,
+        };
+    }
+    return config;
+}
