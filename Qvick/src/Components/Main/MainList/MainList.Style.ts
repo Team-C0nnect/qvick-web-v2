3bf338d189cd7be44e7d9ea38669c2c97a22ad35
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-05-17 15:01:28 +0900

    :wrench: Fix :: tsx ts화

--- Qvick/src/Components/Main/MainList/MainList.Style.ts
+++ Qvick/src/Components/Main/MainList/MainList.Style.ts
@@ -5,16 +5,20 @@ export const MainList = styled.div`
     width: 1400px;
     height: 600px;
     top: 280px;
-    right: 43px;
+    left: 500px;
 `;
 export const MainTable = styled.table`
-    
-`;
-export const MainColgroup = styled.colgroup`
-    
+    position: relative;
+    width: 1400px;
+    height: 680px;
+    right: 233px;
+    bottom: 80px;
 `;
 export const MainThread = styled.thead`
-    
+    position: relative;
+    height: 50px;
+    right: 15px;
+    border-bottom: 1px solid #000000;
 `;
 export const MainTbody = styled.tbody`
     
