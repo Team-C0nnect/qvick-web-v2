f3b3fd003f081bc3c12cc57f9501215f1d647341
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-05-23 08:19:35 +0900

    :zap: Fix :: img 제거 및 style 수정

--- Qvick/src/Components/Head/Logo/Logo.Style.tsx
+++ Qvick/src/Components/Head/Logo/Logo.Style.tsx
@@ -1,22 +1,19 @@
 import styled from "styled-components";
 
 export const LogoWrap = styled.div`
-    display: flex;
-    width: 199px;
+    width: 198px;
     height: 80px;
     background-color: #FFFFFF;
+    display: flex;
     border-right: 1px solid #D9D9D9;
+    align-items: center;
+    justify-content: center;
+
 `;
 export const Logo = styled.img`
-    display: flex;
+    /* display: flex; */
     width: 75px;
     height: 70px;
-    margin-top: 5px;
-    margin-left: 20px;
-`;
-export const Ch = styled.img`
-    display: flex;
-    width: 85px;
-    height: 60px;
-    margin-top: 10px;
+    /* margin-top: 5px;
+    margin-left: 20px; */
 `;
--- Qvick/src/Components/Head/Logo/Logo.tsx
+++ Qvick/src/Components/Head/Logo/Logo.tsx
@@ -1,12 +1,10 @@
 import * as S from "src/Components/Head/Logo/Logo.Style"
 import AppLogo from "src/Assets/images/Head/appLogo.png"
-import Ch from "src/Assets/images/Head/CH.png";
 
 export default function Logo() {
     return (
         <S.LogoWrap>
             <S.Logo src={AppLogo} alt="이미지"></S.Logo>
-            <S.Ch src={Ch} alt="이미지"></S.Ch>
         </S.LogoWrap>
     )
 }
