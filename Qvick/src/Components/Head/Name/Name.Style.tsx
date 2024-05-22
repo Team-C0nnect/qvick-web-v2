0ccf7ef7181414fb46adca9e69e9a14f6ad32715
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-05-23 08:33:51 +0900

    :lipstick: Fix :: style 수정

--- Qvick/src/Components/Head/Name/Name.Style.tsx
+++ Qvick/src/Components/Head/Name/Name.Style.tsx
@@ -1,28 +1,19 @@
 import styled from "styled-components";
 
 export const NameWrap = styled.div`
-    position: relative;
     width: 260px;
     height: 80px;
     background-color: #FFFFFF;
     border-left: 1px solid #D9D9D9;
     border-bottom: 1px solid #D9D9D9;
-    right: 1;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `;
 export const Name = styled.div`
-    position: absolute;
-    margin-top: 20px;
-    margin-left: 35px;
-    width: 100px;
-    height: 30px;
     font-size: 25px;
-    font-weight: 30px;
+    margin-right: 30px;
 `;
-export const logoOut = styled.div`
-    position: absolute;
+export const logOut = styled.div`
     color: #D9D9D9;
-    margin-top: 25px;
-    margin-left: 170px;
-    width: 80px;
-    height: 30px;
 `;
