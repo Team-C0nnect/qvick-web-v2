58d367e00eb986eba2242e48ecc72c9af7bdd4e6
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-05-23 08:33:36 +0900

    :zap: Fix :: logout경로수정

--- Qvick/src/Components/Head/Name/Name.tsx
+++ Qvick/src/Components/Head/Name/Name.tsx
@@ -1,19 +1,18 @@
 import { useNavigate } from "react-router-dom";
 import React from "react";
 import * as S from "src/Components/Head/Name/Name.Style"
-import Login from "src/Components/Login/Login";
 
 export default function Name() {
     const navigate = useNavigate();
     return(
         <S.NameWrap>
-            <S.Name>관리자님</S.Name>
-            <S.logoOut
+            <S.Name>안영세님</S.Name>
+            <S.logOut
                 onClick={() => {
-                    navigate("/")
+                    navigate("/login")
                 }}>
                 로그아웃
-            </S.logoOut>
+            </S.logOut>
         </S.NameWrap>
     )
 }
