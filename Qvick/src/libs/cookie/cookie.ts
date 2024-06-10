2b817c7bda8e83954e42d450aca8a479fc08e128
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-06-10 23:07:27 +0900

    :zap: Fix :: cookie

--- Qvick/src/libs/cookie/cookie.ts
+++ Qvick/src/libs/cookie/cookie.ts
@@ -1,12 +1,30 @@
+// import cookie from "js-cookie";
+
+// class Cookie {
+//     public getCookie(key: string): string | undefined {
+//         let item = undefined;
+//         if (cookie.get(key) !== undefined) {
+//             item = cookie.get(key);
+//         }
+//         return item;
+//     }
+
+//     public setCookie(key: string, value: string): void {
+//         cookie.set(key, value);
+//     }
+
+//     public removeCookie(key: string): void {
+//         cookie.remove(key);
+//     }
+// }
+
+// export default new Cookie();
+
 import cookie from "js-cookie";
 
 class Cookie {
     public getCookie(key: string): string | undefined {
-        let item = undefined;
-        if (cookie.get(key) !== undefined) {
-            item = cookie.get(key);
-        }
-        return item;
+        return cookie.get(key);
     }
 
     public setCookie(key: string, value: string): void {
