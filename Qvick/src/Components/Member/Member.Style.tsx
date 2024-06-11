5ee97ca4476dc16f9d57bc723cee4770878f9661
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-06-11 12:18:56 +0900

    :fire: Fix :: 불필요코드 제거

--- Qvick/src/Components/Member/Member.Style.tsx
+++ Qvick/src/Components/Member/Member.Style.tsx
@@ -1,19 +1,7 @@
 import styled from "styled-components";
 
 export const MemberWrap = styled.div`
-    
-`;
-export const Title = styled.div`
-    position: absolute;
-    color: #2E335D;
-    top: 135px;
-    left: 300px;
-    font-size: 25px;
-    font-weight: 60px;
-`;
-export const Footer = styled.div`
-    position: absolute;
-    color: #D9D9D9;
-    top: 925px;
-    left: 800px;
+    /* display: flex; */
+    width: 100vw;
+    height: 100vh;
 `;
--- Qvick/src/Components/Member/Member.tsx
+++ Qvick/src/Components/Member/Member.tsx
@@ -1,21 +1,14 @@
-import React, { useState, useEffect, ChangeEvent, KeyboardEvent } from "react";
 import * as S from "src/Components/Member/Member.Style"
-import axios from 'axios';
 import Head from "src/Components/Head/Head/Head";
 import SideBar from "src/Components/SideBar/Sidebar";
-import Back from "src/Components/Back/Back";
-import ListBack from "src/Components/ListBack/ListBack";
-
+import MemberList from "src/Components/Member/MemberList/index";
 
 export default function Member() {
     return (
         <S.MemberWrap>
             <Head />
             <SideBar />
-            <Back />
-            <ListBack />
-            <S.Title>구성원 관리</S.Title>
-            <S.Footer>2024 DGSW.School C0nnect</S.Footer>
+            <MemberList />
         </S.MemberWrap>
     );
 }
