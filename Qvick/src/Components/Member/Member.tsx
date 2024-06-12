83b39447fd7ab3a2dbe820ba5cc704527fb840dc
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-06-13 03:40:27 +0900

    :zap: Fix :: SCSS화

--- Qvick/src/Components/Member/Member.tsx
+++ Qvick/src/Components/Member/Member.tsx
@@ -1,15 +1,14 @@
-import * as S from "src/Components/Member/Member.Style"
+import "src/Assets/Scss/member/style.scss";
 import Head from "src/Components/Head/Head/Head";
 import SideBar from "src/Components/SideBar/Sidebar";
 import MemberList from "src/Components/Member/MemberList/index";
-import * as XLSX from 'xlsx';
 
 export default function Member() {
     return (
-        <S.MemberWrap>
+        <div className="MemberWrap">
             <Head />
             <SideBar />
             <MemberList />
-        </S.MemberWrap>
+        </div>
     );
 }
