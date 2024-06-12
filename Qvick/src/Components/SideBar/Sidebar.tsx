0df0c1eede048888595ad775a3664d3c68627ea0
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-06-13 03:57:25 +0900

    :zap: Fix :: SCSS화

--- Qvick/src/Components/SideBar/Sidebar.tsx
+++ Qvick/src/Components/SideBar/Sidebar.tsx
@@ -1,59 +1,58 @@
-import * as S from "src/Components/SideBar/Sidebar.Style"
 import { useNavigate } from "react-router-dom";
-import React from 'react';
 import home from "src/Assets/images/SideBar/home.png";
 import out from "src/Assets/images/SideBar/out.png";
 import check from "src/Assets/images/SideBar/check.png";
 import go from "src/Assets/images/SideBar/go.png";
+import "src/Assets/Scss/sideBar/style.scss"
 
 export default function SideBar() {
     const navigate = useNavigate();
     return (
-        <S.SideWrap>
-            <S.SideM>
-                 <S.moveWrap
+        <div className="sideWrap">
+            <div className="sideM">
+                 <div className="moveWrap"
                     onClick={() => {
                         navigate("/Main")
                     }}>
                     <img src={home} alt="이미지"></img>
                       <span>구성원</span>
-                </S.moveWrap>
-                <S.moveWrap
+                </div>
+                <div className="moveWrap"
                     onClick={() => {
                         navigate("/Check")
                     }}>
                     <img src={check} alt="이미지"></img>
                     <span>출석</span>
-                </S.moveWrap>
-                <S.moveWrap  
+                </div>
+                <div className="moveWrap"  
                     onClick={() => {
                         navigate("/NotCheck")
                     }}>
                     <img src={check} alt="이미지"></img>
                     <span>미출석</span>
-                </S.moveWrap>
-                <S.moveWrap
+                </div>
+                <div className="moveWrap"
                     onClick={() => {
                         navigate("/OutMember")
                     }}>
                     <img src={go} alt="이미지"></img>
                     <span>외출</span>
-                </S.moveWrap>
-                <S.moveWrap
+                </div>
+                <div className="moveWrap"
                     onClick={() => {
                         navigate("/HomeMember")
                     }}>
                     <img src={out} alt="이미지"></img>
                     <span>외박</span>
-                </S.moveWrap>
-                <S.moveWrap
+                </div>
+                <div className="moveWrap"
                     onClick={() => {
                          navigate("/Approve")
                     }}>
                     <img src={check} alt="이미지"></img>
                     <span>승인</span>
-                </S.moveWrap>
-             </S.SideM>
-        </S.SideWrap>
+                </div>
+             </div>
+        </div>
     )
 }
