85b7f8c916b3e31b8714fa3d6d9dece0de42efcd
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-06-15 00:48:19 +0900

    :bug: Fix :: 로그인버튼 기능 활성화
    로그인버튼을 누를려면 이메일과 비밀번호를 모두 입력해야 클릭이 가능하도록 변경

--- Qvick/src/Components/Login/Login.tsx
+++ Qvick/src/Components/Login/Login.tsx
@@ -4,53 +4,51 @@ import * as s from "src/Components/Login/Style";
 import Uselogin from "src/hooks/auth/UseLogin";
 
 export default function Login() {
-  const {...login}=Uselogin();
+  const { idValue, passwordValue, InputChange, InputChangePw, LoginButton } = Uselogin();
   const [emailValid, setEmailValid] = useState(false);
   const [pwValid, setPwValid] = useState(false);
   const [notAllow, setNotAllow] = useState(true);
 
-
-  useEffect(() => { // 버튼활성화 기능 필요 사용시 변경가능
-    if (emailValid && pwValid) {
-      setNotAllow(false);
+  useEffect(() => {
+    if (idValue !== "" && passwordValue !== "") {
+      setEmailValid(true);
+      setPwValid(true);
     } else {
-      setNotAllow(false);
+      setEmailValid(false);
+      setPwValid(false);
     }
-  }, [emailValid, pwValid]);
+    setNotAllow(!(emailValid && pwValid));
+  }, [idValue, passwordValue, emailValid, pwValid]);
 
   return (
     <s.AllPage>
       <s.LoginPage>
         <s.LoginTitle>
-          <s.TitleImg src={Title} alt = "이미지"></s.TitleImg>
+          <s.TitleImg src={Title} alt="이미지"></s.TitleImg>
         </s.LoginTitle>
         <s.ContentWrap>
-          
-            <s.Input
-              type="text"
-              placeholder="이메일을 입력해주세요."
-              value={login.idValue}
-              onChange={login.InputChange}
-            />
-
-            <s.Input
-              type="password"
-              placeholder="비밀번호를 입력해주세요."
-              value={login.passwordValue}
-              onChange={login.InputChangePw}
-            />
+          <s.Input
+            type="text"
+            placeholder="이메일을 입력해주세요."
+            value={idValue}
+            onChange={InputChange}
+          />
+          <s.Input
+            type="password"
+            placeholder="비밀번호를 입력해주세요."
+            value={passwordValue}
+            onChange={InputChangePw}
+          />
         </s.ContentWrap>
         <s.LoginButtonWrap>
           <s.LoginButton
-            onClick={login.LoginButton}
+            onClick={LoginButton}
             disabled={notAllow}
             style={{ fontSize: notAllow ? "initial" : "20px" }}
           >
             로그인
           </s.LoginButton>
         </s.LoginButtonWrap>
-        
-        
       </s.LoginPage>
     </s.AllPage>
   );
