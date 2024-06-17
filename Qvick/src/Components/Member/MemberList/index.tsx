4d38f81f91ea980a83f70a68dfd32c4bf418e194
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-06-17 17:31:09 +0900

    :bug: Fix :: 서버리스폰스 변경에 의한 수정

--- Qvick/src/Components/Member/MemberList/index.tsx
+++ Qvick/src/Components/Member/MemberList/index.tsx
@@ -1,29 +1,31 @@
 import React, { useEffect, useState, useMemo } from "react";
 import { MemberType } from "src/types/Member/member.type";
 import { qvickV1Axios } from "src/libs/auth/CustomAxios";
+import { AxiosError } from 'axios';
 import * as XLSX from 'xlsx';
-import "src/Assets/Scss/memberList/style.scss"
+import "src/Assets/Scss/memberList/style.scss";
 
 const MemberList = () => {
-    const [memberList, setMemberList] = useState<MemberType[]>([]);
+    const [memberList, setMemberList] = useState<MemberType['data'][]>([]);
     const [isLoading, setIsLoading] = useState(true);
     const [error, setError] = useState<Error | null>(null);
 
     const fetchMemberList = async () => {
+        setIsLoading(true);
+        setError(null);
         try {
             const response = await qvickV1Axios.get('user-admin/student-all', {
                 params: { page: 1, size: 1000 },
             });
-            const sortedData = response.data.sort((a: MemberType, b: MemberType) => a.stdId - b.stdId); // 학번 기준 정렬
+
+            const data = response.data.data; // 응답 데이터에서 실제 데이터를 추출
+            const sortedData = data.sort((a: MemberType['data'], b: MemberType['data']) => parseInt(a.stdId) - parseInt(b.stdId)); // 학번 기준 정렬
             setMemberList(sortedData);
             console.log("성공");
         } catch (error) {
-            if (error instanceof Error) {
-                setError(error);
-            } else {
-                setError(new Error("Unknown error"));
-            }
-            console.error("실패", error);
+            const axiosError = error as AxiosError;
+            console.error("실패", axiosError);
+            setError(axiosError);
         } finally {
             setIsLoading(false);
         }
@@ -61,7 +63,7 @@ const MemberList = () => {
     );
 };
 
-const Table = ({ data }: { data: MemberType[] }) => (
+const Table = ({ data }: { data: MemberType['data'][] }) => (
     <>
         <div className="thead">
             <div className="thead-tr">
