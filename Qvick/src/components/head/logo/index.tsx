c505312ad1a44c7cb285ba57c389f7eef3dc90a5
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-06-21 21:00:55 +0900

    :zap: Fix :: svg 변경

--- Qvick/src/components/head/logo/index.tsx
+++ Qvick/src/components/head/logo/index.tsx
@@ -1,10 +1,10 @@
 import * as S from "src/components/head/logo/Logo.Style"
-import AppLogo from "src/assets/images/head/appLogo.png"
+import appLogo from "src/assets/images/head/logo.svg";
 
 export default function Logo() {
     return (
         <S.LogoWrap>
-            <S.Logo src={AppLogo} alt="이미지"></S.Logo>
+            <S.Logo src={appLogo} alt="이미지"></S.Logo>
         </S.LogoWrap>
     )
 }
