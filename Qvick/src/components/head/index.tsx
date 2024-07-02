686e67083fea22e71c0bf636019137cd2bec55af
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-07-02 20:21:25 +0900

    :bug: Fix :: ES6 모듈에러

--- Qvick/src/components/head/index.tsx
+++ Qvick/src/components/head/index.tsx
@@ -2,6 +2,7 @@ import * as S from "src/components/head/Head.Style"
 import HeadLogo from "src/components/head/logo/index";
 import Situation from "src/components/head/situation/index";
 import HeadName from "src/components/head/name/index";
+import React from "react";
 
 export default function Head() {
     return (
