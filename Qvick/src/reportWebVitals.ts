54614d968da587d6e91ec0cf67edbe9464557f65
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-06 22:04:32 +0900

    docs :: reportWebVitals 문서화

--- Qvick/src/reportWebVitals.ts
+++ Qvick/src/reportWebVitals.ts
@@ -1,6 +1,9 @@
 const reportWebVitals = onPerfEntry => {
+  // onPerfEntry가 함수인지 확인
   if (onPerfEntry && onPerfEntry instanceof Function) {
+    // 'web-vitals' 라이브러리를 동적으로 import
     import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+      // 성능 지표를 측정하고 결과를 onPerfEntry 콜백에 전달
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
