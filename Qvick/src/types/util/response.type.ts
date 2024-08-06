b5d2f33ad53cbfb25fbb7ab21b16dcd3c99b25a1
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-06 22:33:00 +0900

    docs :: response type 문서화

--- Qvick/src/types/util/response.type.ts
+++ Qvick/src/types/util/response.type.ts
@@ -1,4 +1,9 @@
+// Response 인터페이스를 정의합니다.
 export interface Response {
-    status: string,
-    message: string
+    status: string;
+    // status: 응답 상태를 나타내는 문자열입니다.
+    // 예를 들어, "success", "error" 등의 값을 가짐
+
+    message: string;
+    // message: 응답에 대한 추가 설명이나 메시지를 제공하는 문자열
 }
