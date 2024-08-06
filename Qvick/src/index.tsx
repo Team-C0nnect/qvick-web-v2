ab37766f833bb66d1758a11588f74d4ef2e07ae2
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-06 22:04:39 +0900

    docs :: index 문서화

--- Qvick/src/index.tsx
+++ Qvick/src/index.tsx
@@ -1,16 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// React 및 ReactDOM 모듈을 가져옴
+import React from 'react'; // React 라이브러리로, 컴포넌트 및 JSX 문법을 사용
+import ReactDOM from 'react-dom/client'; // React 컴포넌트를 실제 DOM에 렌더링하는 데 사용
+import App from './App'; // 애플리케이션의 메인 컴포넌트를 가져옴
+import reportWebVitals from './reportWebVitals'; // 웹 성능 지표를 측정하는 함수를 가져옴
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')); // React 애플리케이션의 루트 DOM 노드를 생성
+
+// 애플리케이션을 'root' DOM 노드에 렌더링
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    // React.StrictMode로 감싸서 렌더링하여 잠재적인 문제를 발견할 수 있게 함
+    <React.StrictMode>
+        <App /> {/* 메인 애플리케이션 컴포넌트를 렌더링 */}
+    </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
+// 웹 성능 지표를 측정하여 콘솔에 로그를 남김
+reportWebVitals(console.log); // 측정된 성능 지표를 콘솔에 출력
