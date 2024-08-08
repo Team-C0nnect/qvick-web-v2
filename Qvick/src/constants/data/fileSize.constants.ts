35e8eb5ac03a85b216a38f79f85bd12e9f4edf2f
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-08 21:39:27 +0900

    docs :: constants file 문서화

--- Qvick/src/constants/data/fileSize.constants.ts
+++ Qvick/src/constants/data/fileSize.constants.ts
@@ -1,1 +1,3 @@
-export const FILE_SIZES = ["B", "KB", "MB", "GB", "TB"] as const
+// 파일 크기를 나타내는 단위들을 상수 배열로 정의
+// "B"는 바이트, "KB"는 킬로바이트, "MB"는 메가바이트, "GB"는 기가바이트, "TB"는 테라바이트를 의미
+export const FILE_SIZES = ["B", "KB", "MB", "GB", "TB"] as const;
