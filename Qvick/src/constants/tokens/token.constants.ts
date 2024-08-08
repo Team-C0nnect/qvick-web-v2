e019ab67df16b53677358b626a45556108005071
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-08 21:39:21 +0900

    docs :: constants token 문서화

--- Qvick/src/constants/tokens/token.constants.ts
+++ Qvick/src/constants/tokens/token.constants.ts
@@ -1,3 +1,8 @@
+// 액세스 토큰을 저장할 때 사용하는 키
 export const ACCESS_TOKEN_KEY = "accessToken";
+
+// 리프레시 토큰을 저장할 때 사용하는 키
 export const REFRESH_TOKEN_KEY = "refreshToken";
+
+// HTTP 요청 헤더에 사용하는 토큰 키 일반적으로 Authorization 헤더에 사용
 export const REQUEST_TOKEN_KEY = "Authorization";
