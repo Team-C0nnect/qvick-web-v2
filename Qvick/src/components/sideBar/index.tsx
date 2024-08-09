36e1f1b0716934eb96d805c90efae719f2eb9694
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-09 22:27:58 +0900

    docs :: Sidebar 문서화

--- Qvick/src/components/sideBar/index.tsx
+++ Qvick/src/components/sideBar/index.tsx
@@ -1,36 +1,40 @@
-import { useNavigate } from "react-router-dom";
-import home from "src/assets/images/sideBar/home.png";
-import check from "src/assets/images/sideBar/check.png";
-import "src/assets/scss/sideBar/style.scss"
-import React from "react";
+import { useNavigate } from "react-router-dom"; // 페이지 이동을 위한 useNavigate 훅을 가져옴
+import home from "src/assets/images/sideBar/home.png"; // '구성원' 메뉴에 사용할 홈 이미지를 가져옴
+import check from "src/assets/images/sideBar/check.png"; // '출석' 및 '미출석' 메뉴에 사용할 체크 이미지를 가져옴
+import "src/assets/scss/sideBar/style.scss"; // 사이드바 스타일을 위한 SCSS 파일을 가져옴
+import React from "react"; // React를 가져옴
 
 export default function SideBar() {
-    const navigate = useNavigate();
+    const navigate = useNavigate(); // 페이지 이동을 위한 네비게이트 훅을 초기화
+
     return (
-        <div className="sideWrap">
-            <div className="sideM">
-                 <div className="moveWrap"
-                    onClick={() => {
-                        navigate("/Main")
-                    }}>
-                    <img src={home} alt="이미지"></img>
-                      <span>구성원</span>
-                </div>
+        <div className="sideWrap"> {/* 사이드바 전체를 감싸는 컨테이너 */}
+            <div className="sideM"> {/* 메뉴를 감싸는 컨테이너 */}
+                {/* '구성원' 메뉴 항목 */}
                 <div className="moveWrap"
-                    onClick={() => {
-                        navigate("/Check")
-                    }}>
-                    <img src={check} alt="이미지"></img>
-                    <span>출석</span>
+                     onClick={() => {
+                         navigate("/Main"); // '구성원' 메뉴를 클릭하면 "/Main" 페이지로 이동
+                     }}>
+                    <img src={home} alt="이미지" /> {/* 홈 이미지를 표시 */}
+                    <span>구성원</span> {/* 메뉴 이름 */}
                 </div>
-                <div className="moveWrap"  
-                    onClick={() => {
-                        navigate("/NotCheck")
-                    }}>
-                    <img src={check} alt="이미지"></img>
-                    <span>미출석</span>
+                {/* '출석' 메뉴 항목 */}
+                <div className="moveWrap"
+                     onClick={() => {
+                         navigate("/Check"); // '출석' 메뉴를 클릭하면 "/Check" 페이지로 이동
+                     }}>
+                    <img src={check} alt="이미지" /> {/* 체크 이미지를 표시 */}
+                    <span>출석</span> {/* 메뉴 이름 */}
                 </div>
-             </div>
+                {/* '미출석' 메뉴 항목 */}
+                <div className="moveWrap"
+                     onClick={() => {
+                         navigate("/NotCheck"); // '미출석' 메뉴를 클릭하면 "/NotCheck" 페이지로 이동
+                     }}>
+                    <img src={check} alt="이미지" /> {/* 체크 이미지를 표시 */}
+                    <span>미출석</span> {/* 메뉴 이름 */}
+                </div>
+            </div>
         </div>
-    )
+    );
 }
