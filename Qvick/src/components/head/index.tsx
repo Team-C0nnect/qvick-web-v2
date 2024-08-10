5d7b8c1fbdb2420e75c5d1b82036abcdd51f72b0
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-10 21:19:25 +0900

    docs :: head 페이지 문서화

--- Qvick/src/components/head/index.tsx
+++ Qvick/src/components/head/index.tsx
@@ -1,15 +1,17 @@
-import * as S from "src/components/head/Head.Style"
-import HeadLogo from "src/components/head/logo/index";
-import Situation from "src/components/head/situation/index";
-import HeadName from "src/components/head/name/index";
-import React from "react";
+import * as S from "src/components/head/Head.Style"; // 스타일드 컴포넌트 가져오기
+import HeadLogo from "src/components/head/logo/index"; // 헤더 로고 컴포넌트 가져오기
+import Situation from "src/components/head/situation/index"; // 상태 컴포넌트 가져오기
+import HeadName from "src/components/head/name/index"; // 헤더 이름 컴포넌트 가져오기
+import React from "react"; // React 라이브러리 가져오기
 
+// Head 컴포넌트 정의
 export default function Head() {
     return (
+        // 스타일드 컴포넌트로 감싸진 헤더
         <S.Head>
-            <HeadLogo />
-            <Situation />
-            <HeadName />
+            <HeadLogo /> {/* 로고 컴포넌트 */}
+            <Situation /> {/* 상태 컴포넌트 */}
+            <HeadName /> {/* 이름 컴포넌트 */}
         </S.Head>
     )
 }
