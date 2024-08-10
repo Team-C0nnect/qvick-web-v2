7f2175a57a2554d666c64032836c7b6d52ebc36f
박규민 <131234408+kyumin7487@users.noreply.github.com>
2024-08-10 21:20:03 +0900

    docs :: head logo 문서화

--- Qvick/src/components/head/logo/index.tsx
+++ Qvick/src/components/head/logo/index.tsx
@@ -1,10 +1,12 @@
-import * as S from "src/components/head/logo/Logo.Style"
-import appLogo from "src/assets/images/head/logo.svg";
+import React from "react"; // React 불러오기
+import * as S from "src/components/head/logo/Logo.Style"; // 스타일 컴포넌트 불러오기
+import appLogo from "src/assets/images/head/logo.svg"; // 로고 이미지 파일 불러오기
 
+// Logo 컴포넌트 정의
 export default function Logo() {
     return (
-        <S.LogoWrap>
-            <S.Logo src={appLogo} alt="이미지"></S.Logo>
+        <S.LogoWrap> {/* 스타일이 적용된 LogoWrap 컴포넌트 */}
+            <S.Logo src={appLogo} alt="이미지" /> {/* 로고 이미지 표시 */}
         </S.LogoWrap>
-    )
+    );
 }
